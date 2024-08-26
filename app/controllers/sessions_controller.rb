acfda6ad66a0721c6d873e05ce0c1cf25f0dc0e2
Gabriella Massaro <gmassaro807@gmail.com>
2024-08-26 11:59:02 -0400

    All tests are passing but the app doesn't work

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,8 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-    @user = User.find_by(name: params[:name])
-    if @user && @user.authenticate(params[:password])
+    @user = User.find_by(name: params[:user][:name])
+    if @user && @user.authenticate(params[:user][:password])
       session[:user_id] = @user.id
       redirect_to root_path
     else
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'application#hello'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
   resources :users, only: [:new, :create]
-  resources :sessions, only: [:index, :show, :create, :new, :destroy]
-
-  get '/login', to: 'sessions#new'
 end
