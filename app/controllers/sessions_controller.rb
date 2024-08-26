e58c88efc18f2e6647d7f7f485cceb6c2c6f4554
Gabriella Massaro <gmassaro807@gmail.com>
2024-08-26 11:09:56 -0400

    All tests but one are passing

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,22 +1,19 @@
 class SessionsController < ApplicationController
   def new
-  end
+	end
 
-  def create
+	def create
     @user = User.find_by(name: params[:name])
-    return head(:forbidden) unless @user.authenticate(params[:password])
-    session[:user_id] = @user.id
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      redirect_to '/login'
+    end
+	end
+
+  def destroy
+    session.delete :name
+    redirect_to '/login'
   end
-
-  # def new
-	# end
-
-	# def create
-  #   if params[:name].present?
-  #     session[:name] = params[:name]
-  #     redirect_to root_path
-  #   else
-  #     redirect_to '/login'
-  #   end
-	# end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root 'application#hello'
   resources :users, only: [:new, :create]
+  resources :sessions, only: [:index, :show, :create, :new, :destroy]
+
+  get '/login', to: 'sessions#new'
 end
