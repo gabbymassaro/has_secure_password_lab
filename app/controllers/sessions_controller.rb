b1cb118d8aac21d7ae55b231d4688b3adefbb859
Gabriella Massaro <gmassaro807@gmail.com>
2024-08-26 10:41:58 -0400

    UsersController tests pass

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,22 @@
 class SessionsController < ApplicationController
+  def new
+  end
+
   def create
-    @user = User.find_by(username: params[:username])
+    @user = User.find_by(name: params[:name])
     return head(:forbidden) unless @user.authenticate(params[:password])
     session[:user_id] = @user.id
   end
+
+  # def new
+	# end
+
+	# def create
+  #   if params[:name].present?
+  #     session[:name] = params[:name]
+  #     redirect_to root_path
+  #   else
+  #     redirect_to '/login'
+  #   end
+	# end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,22 @@ class UsersController < ApplicationController
   end
 
   def create
-    User.create(user_params)
+    if params[:password] != params[:password_confirmation]
+      render :new
+    else
+      @user = User.new(user_params)
+      if @user.save
+        session[:user_id] = @user.id
+        redirect_to root_path
+      else
+        redirect_to new_user_path
+      end
+    end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:username, :password, :password_confirmation)
+    params.require(:user).permit(:name, :password, :password_confirmation)
   end
 end
